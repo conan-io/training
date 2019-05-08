0b39535c2275ea582c8a58c3d64f7b6402d7dca2
memsharded <james@conan.io>
2019-05-08 10:30:39 +0000

    scm

---
+++ scm/conanfile.py
@@ -0,0 +1,34 @@
+from conans import ConanFile, CMake
+
+
+class HelloConan(ConanFile):
+    name = "Hello"
+    version = "0.1"
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Hello here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+    scm = {"type": "git",
+           "url": "auto",
+           "revision": "auto"}
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="scm/src")
+        cmake.build()
+
+    def package(self):
+        self.copy("*.h", dst="include", src="scm/src")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.dylib*", dst="lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["hello"]
