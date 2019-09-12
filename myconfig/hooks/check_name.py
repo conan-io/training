ab5acbe7a72771e9ee64216c519659948c14cf9b
memsharded <james@conan.io>
2019-09-12 16:15:41 +0200

    hooks

--- myconfig/hooks/check_name.py
+++ myconfig/hooks/check_name.py
@@ -6,3 +6,4 @@ def pre_export(output, conanfile, conanfile_path, reference, **kwargs):
     if ref.lower() != ref:
         raise Exception("Reference %s should be all lowercase" % ref)
     # use python "in" syntax.... if "something" in ref:
+    #TODO
