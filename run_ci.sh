65580a6dec8500d7785af1afc51e94bfa4f6ffe2
danimtb <danimanzaneque@gmail.com>
2020-04-07 11:49:02 +0200

    make it error

--- run_ci.sh
+++ run_ci.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-set -e && ./catchup.sh 1
-set -e && ./catchup.sh 2
-set -e && ./catchup.sh 3
-set -e && ./catchup.sh 4
-set -e && ./catchup.sh 5
-set -e && ./catchup.sh 6
-set -e && ./catchup.sh 7
-set -e && ./catchup.sh 8
-set +e && ./catchup.sh 9    # requires Artifactory
-set -e && ./catchup.sh 10
-set +e && ./catchup.sh 11   # requires Artifactory
-set -e && ./catchup.sh 12
-set -e && ./catchup.sh 13
-set +e && ./catchup.sh 14   # option error
-set +e && ./catchup.sh 15   # requires xbuilding
-set +e && ./catchup.sh 16   # requires xbuilding
-set -e && ./catchup.sh 17   # requires conflict
-set -e && ./catchup.sh 18
-set -e && ./catchup.sh 19
-set -e && ./catchup.sh 20
-set -e && ./catchup.sh 21
-set -e && ./catchup.sh 22
-set -e && ./catchup.sh 23
-set -e && ./catchup.sh 24
-set -e && ./catchup.sh 25
-set -e && ./catchup.sh 26
+set -e
+./catchup.sh 1
+./catchup.sh 2
+./catchup.sh 3
+./catchup.sh 4
+./catchup.sh 5
+./catchup.sh 6
+./catchup.sh 7
+./catchup.sh 8
+#set +e && ./catchup.sh 9    # requires Artifactory
+./catchup.sh 10
+#set +e && ./catchup.sh 11   # requires Artifactory
+./catchup.sh 12
+./catchup.sh 13
+#set +e && ./catchup.sh 14   # option error
+#set +e && ./catchup.sh 15   # requires xbuilding
+#set +e && ./catchup.sh 16   # requires xbuilding
+#set +e && ./catchup.sh 17   # requires conflict
+./catchup.sh 17  # should fail
+./catchup.sh 18
+./catchup.sh 19
+./catchup.sh 20
+./catchup.sh 21
+./catchup.sh 22
+./catchup.sh 23
+./catchup.sh 24
+./catchup.sh 25
+./catchup.sh 26
