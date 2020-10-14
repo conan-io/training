24cef6581c9e7f5537b983d6e53c34d8e896611a
Jerry Wiltse <jerrywiltse@gmail.com>
2020-10-14 14:43:10 -0400

    rename server in docker-compose, also add yaml content to create a conan repo to try to force bypass of first-run-wizard

--- docker_environment/artifactory-training/scripts/set-internal-hostname.sh
+++ docker_environment/artifactory-training/scripts/set-internal-hostname.sh
@@ -7,4 +7,8 @@ curl -m 5 -f -X PATCH \
 http://localhost:8082/artifactory/api/system/configuration \
 -d '
 urlBase: http://jfrog-artifactory-training:8082
+localRepositories: #Local repositories configuration
+     conan-tmp: #The local repository name
+          type: conan #The package type
 '
+# we also create a repository here to bypass the first-run wizard
