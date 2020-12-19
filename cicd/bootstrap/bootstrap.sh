668971b3c1187f980252b94f13d57763ec747942
Jerry Wiltse <jerrywiltse@gmail.com>
2020-12-19 00:35:50 -0500

    update docker strategy to mount training directory into container rather than checking out a copy when building it, fix cicd scripts to work as a result

--- cicd/bootstrap/bootstrap.sh
+++ cicd/bootstrap/bootstrap.sh
@@ -8,21 +8,9 @@ TRAINING_JENKINS_URL=${TRAINING_JENKINS_URL:-http://jenkins:8080}
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 
-JENKINSLIB_SRC=~/training/cicd/automation/jenkinslib
-SCRIPTS_SRC=~/training/cicd/automation/scripts
-DATA_SRC=~/training/cicd/data
-
-WORKSPACE=~/workspace
-SCRIPTS=~/scripts
-JENKINSLIB=~/jenkinslib
-
-mkdir -p "${WORKSPACE}"
-mkdir -p "${SCRIPTS}"
-mkdir -p "${JENKINSLIB}"
-
-cp -RT "${DATA_SRC}" "${WORKSPACE}"
-cp -RT "${SCRIPTS_SRC}" "${SCRIPTS}"
-cp -RT "${JENKINSLIB_SRC}" "${JENKINSLIB}"
+JENKINSLIB=~/training/cicd/automation/jenkinslib
+SCRIPTS=~/training/cicd/automation/scripts
+DATA=~/training/cicd/data
 
 initialize_repo(){
     echo "--- creating GIT repository on server: ${1}"
@@ -33,7 +21,6 @@ initialize_repo(){
     curl --user "${TRAINING_GIT_CREDS_USR}:${TRAINING_GIT_CREDS_PSW}" \
     -X POST "${TRAINING_GIT_URL}/api/v3/repos/${TRAINING_GIT_CREDS_USR}/${1}/hooks" \
     -d '{"name":"jenkins","config":{"url":"'"${TRAINING_JENKINS_URL}/github-webhook/"'"},"events":["create", "push", "pull_request"]}'
-    
     pushd $1
     echo "--- creating GIT repo locally: ${1}"
     git init 
@@ -48,11 +35,11 @@ initialize_repo(){
     popd
 }
 
-cd ~
+cd ~/training
 initialize_repo $(basename $SCRIPTS)
 initialize_repo $(basename $JENKINSLIB)
 
-cd ${WORKSPACE}
+cd ~/training/cicd/data
 for repo in $(ls $WORKSPACE); do
     initialize_repo $repo $WORKSPACE
 done
