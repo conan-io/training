a926e4f754ff278b8b76b092e55a740e93858a74
Jerry Wiltse <jerrywiltse@gmail.com>
2020-12-20 22:50:12 -0500

    fix suspected error in app1's main.cpp file

--- cicd/data/app1/src/main.cpp
+++ cicd/data/app1/src/main.cpp
@@ -8,6 +8,7 @@
 int main() {
     std::cout << "app1: v1.0" << std::endl;
     hello_libb(1, "called from app1");
-    hello_libb(1, "called from app1");    
+    hello_libc(1, "called from app1");    
+    hello_libd(1, "called from app1");    
     return 0;
 }
