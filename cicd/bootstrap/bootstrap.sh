80c41797fd61399a099ae97698d2b233a5f9dc30
Jerry Wiltse <jerrywiltse@gmail.com>
2020-12-30 11:49:05 -0500

    create workspace directory in bootstrap, also add comments to explain odd command and no longer enable create webhooks cuz not needed

--- cicd/bootstrap/bootstrap.sh
+++ cicd/bootstrap/bootstrap.sh
@@ -16,7 +16,7 @@ initialize_repo(){
     
     curl --user "${TRAINING_GIT_CREDS_USR}:${TRAINING_GIT_CREDS_PSW}" \
     -X POST "${TRAINING_GIT_URL}/api/v3/repos/${TRAINING_GIT_CREDS_USR}/${1}/hooks" \
-    -d '{"name":"jenkins","config":{"url":"'"${TRAINING_JENKINS_URL}/github-webhook/"'"},"events":["create", "push", "pull_request"]}'
+    -d '{"name":"jenkins","config":{"url":"'"${TRAINING_JENKINS_URL}/github-webhook/"'"},"events":["push", "pull_request"]}'
     pushd $1
     echo "--- creating GIT repo locally: ${1}"
     rm -rf .git
@@ -26,9 +26,9 @@ initialize_repo(){
     git commit -m "initial commit"
     echo "--- pushing GIT repository: ${1}"
     git remote add origin "${TRAINING_GIT_URL}/git/${TRAINING_GIT_CREDS_USR}/${1}.git"
-    git push origin --mirror -f
-    git checkout -b conan_from_upstream
-    git push origin --mirror -f
+    git push origin --mirror -f # Push once with just develop so it becomes default branch in gitbucket
+    git branch conan_from_upstream
+    git push origin --mirror -f # Push second time to create conan_from_upstream as non-default branch
     popd
 }
 
@@ -36,6 +36,8 @@ initialize_repo(){
 AUTOMATION=~/training/cicd/automation
 DATA=~/training/cicd/data
 WORKSPACE=~/workspace
+
+mkdir -p $WORKSPACE
 
 cd $AUTOMATION
 initialize_repo scripts
@@ -47,3 +49,4 @@ for repo in $(ls); do
 done
 
 ln -s $AUTOMATION/scripts ~/scripts
+
