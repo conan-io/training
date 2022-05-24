6a24c5408bf7b153389e9272d381aa835b212d3e
Uilian Ries <uilianries@gmail.com>
2022-05-24 09:19:29 +0200

    Rever check_name.py
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- myconfig/hooks/check_name.py
+++ myconfig/hooks/check_name.py
@@ -6,5 +6,4 @@ def pre_export(output, conanfile, conanfile_path, reference, **kwargs):
     if ref.lower() != ref:
         raise Exception("Reference %s should be all lowercase" % ref)
     # use python "in" syntax.... if "something" in ref:
-    if '-' in ref:
-        raise Exception('Use _ instead of -')
+    #TODO
